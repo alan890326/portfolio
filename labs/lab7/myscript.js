52b9ca9f5948e63cd6a70b4304793ba47683f06a
alan890326 <40756679+alan890326@users.noreply.github.com>
2018-07-09 17:08:13 +0800

    Add files via upload

---
+++ labs/lab7/myscript.js
@@ -0,0 +1,13 @@
+
+function start()
+{
+	var button = document.getElementById( "calculateButton" );
+	button.addEventListener( "click", displayVolume, false );
+}
+
+function displayVolume()
+{
+	document.getElementById('myId').innerHTML = 'Welcome';
+}
+
+window.addEventListener( "load", start, false);
---
+++ labs/lab8/lab8.js
@@ -0,0 +1,12 @@
+var counter = 0;
+function start()
+{
+	counter++;
+	if (counter%2==0){
+		document.getElementById('seal').src = "./black.png";
+	}else{
+		document.getElementById('seal').src = "./gold.png";
+	}
+}
+setInterval(start, 1000);
+window.addEventListener("load", start, false);
